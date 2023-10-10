3eb29332859d11a279ca6b1cfd421ef7aaf923e8
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-10-10 09:29:35 -0400

    s
    s

---
+++ index.js
@@ -0,0 +1,25 @@
+/* 
+const Lookat = {
+    firstname: "Lookat",
+    lastname: "Disdude",
+};
+const akash = {
+    firstname: "akash",
+    lastname: "disdude",
+}
+
+console.log(Marco,Lookat,akash);
+
+let nums = [Marco, Lookat, akash];
+nums.forEach((students)=> console.log(students)); */
+
+const Igor = {
+    album: "best album", 
+};
+const FlowerBoy = {
+    album: "very close second",
+}
+console.log(Igor, FlowerBoy);
+
+let nums = [Igor, FlowerBoy]
+nums.forEach((albums)=> console.log(albums))
