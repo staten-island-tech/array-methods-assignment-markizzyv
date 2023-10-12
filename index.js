1d35c1932eaff1c02326cb499e877c6504227e16
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-10-12 08:45:32 -0400

    g
    a

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-function albums(){
+function albums()
 const Igor = {
     album: "best album", 
+
 };
 const FlowerBoy = {
     album: "very close second",
 }
-}
+
 
 const tyler = ['Igor', 'Flowerboy'];
 console.log(tyler);
 
+const album = [{}]
