77886617056d39650575fa06748eeaa2c8ab124f
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-10-15 21:31:24 -0400

    s
    s

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 
 const Igor = {
     album: "Igor", 
+    fire: "yes",
+    favsong: "PUPPET",
 
 };
 const FlowerBoy = {
     album: "Flower Boy",
+    fire: "yes",
+    favsong: "911 / Mr. Lonely",
 };
 const CallMeIfYouGetLost = {
     album: "Call Me If You Get Lost",
+    fire: "yes",
+    favsong: "SWEET / I THOUGHT YOU WANTED TO DANCE",
+
 }
 const Wolf = {
-    album: "Wolf"
+    album: "Wolf",
+    fire: "yes",
+    favsong: "Bimmer",
 }
 const Goblin = {
-    album: "Goblin"
-    rating: trasj
+    album: "Goblin",
+    fire: "no",
+    favsong: "She",
 }
 const CherryBomb = {
-    album: "Cherry Bomb"
-    rating: trash
+    album: "Cherry Bomb",
+    fire: "no",
+    favsong: "SMUCKERS",
 }
 
+ let tyler = [ Igor, FlowerBoy, CallMeIfYouGetLost, Wolf, Goblin , CherryBomb ];
+console.log(tyler)
+tyler.forEach((tyler)=> console.log(tyler.album))
+tyler.forEach((tyler)=> console.log(tyler.fire))
 
-
-const tyler = [ 'Igor','Flower Boy', 'Call Me If You Get Lost', 'Wolf', 'Goblin' , 'Cherry Bomb' ];
-console.log(tyler);
-
-const result = tyler.filter((album) => (album = "trash"));
-console.log(result) 
-
-const tylersongs = [ 'SWEET/I THOUGHT YOU WANTED TO DANCE']
+const trash = tyler.filter((tyler)=> tyler.fire === "no")
+console.log(trash)
